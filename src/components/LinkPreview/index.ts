c3639074641842544cb108a8fcf251a0bd3f56aa
dPacc <themdk01@gmail.com>
2026-01-29 14:45:23 +0400

    add: LinkPreview module index exports

---
+++ src/components/LinkPreview/index.ts
@@ -0,0 +1,9 @@
+/**
+ * LinkPreview Component Exports
+ *
+ * Rich link preview cards for URLs in messages
+ */
+
+export { LinkPreview } from './LinkPreview';
+export type { LinkPreviewProps } from './LinkPreview';
+export { default } from './LinkPreview';
