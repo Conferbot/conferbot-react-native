23e3b0bdc3bd205ba43fcc0b8c1216fff987c925
dPacc <themdk01@gmail.com>
2026-01-29 11:32:01 +0400

    add: nodes module index exports

---
+++ src/core/nodes/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Nodes Module Index
+ *
+ * Main entry point for the node handling system.
+ */
+
+// Export types
+export * from './NodeTypes';
+export * from './NodeHandler';
+export { NodeHandlerRegistry, FallbackNodeHandler } from './NodeHandlerRegistry';
+
+// Export handlers
+export * from './handlers';
