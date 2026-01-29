49a4e40a24793dbb471ed13a9b7fda2ed2c77820
dPacc <themdk01@gmail.com>
2026-01-29 14:41:29 +0400

    add: Jest configuration for React Native testing

---
+++ jest.config.js
@@ -0,0 +1,52 @@
+/**
+ * Jest Configuration for Conferbot React Native SDK
+ */
+module.exports = {
+  preset: 'react-native',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/__tests__'],
+  testMatch: [
+    '**/__tests__/**/*.test.ts',
+    '**/__tests__/**/*.test.tsx',
+    '**/src/**/*.test.ts',
+    '**/src/**/*.test.tsx',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        isolatedModules: true,
+      },
+    ],
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-native-async-storage)/)',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+    '!src/types/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  coverageReporters: ['text', 'lcov', 'html'],
+  testPathIgnorePatterns: ['/node_modules/', '/lib/'],
+  globals: {
+    __DEV__: true,
+  },
+  clearMocks: true,
+  restoreMocks: true,
+};
