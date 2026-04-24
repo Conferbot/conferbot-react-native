f34205a0f697c9607ce919bb0c46d036b3cc5bf8
dPacc <themdk01@gmail.com>
2026-04-24 16:35:18 +0400

    add: export HeaderMenu component from ChatHeader index

--- src/components/ChatHeader/index.ts
+++ src/components/ChatHeader/index.ts
@@ -1,2 +1,4 @@
 export { ChatHeader } from './ChatHeader';
 export type { ChatHeaderProps } from './ChatHeader';
+export { HeaderMenu } from './HeaderMenu';
+export type { HeaderMenuItem, HeaderMenuProps } from './HeaderMenu';
